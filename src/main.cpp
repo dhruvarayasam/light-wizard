050037baa822865305606d155cdfa50d418c3530
Dhruva Rayasam <dhruva.rayasam@gmail.com>
2024-01-02 14:27:12 -0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Config.h"
+
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+
+	cout << "ray tracing project" << endl;
+	cout << VERSION_MAJOR << endl;
+	cout << VERSION_MINOR << endl;	
+
+
+	return 0;
+
+}
